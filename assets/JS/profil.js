a8ee31588b2ff47a288346850633cacd16daf3c7
Clairinett_ <claire.crinier@gmail.com>
2023-03-23 16:57:46 +0000

    menu burger semiOK

--- assets/JS/profil.js
+++ assets/JS/profil.js
@@ -1,1 +1,23 @@
-const avatar = document.getElementById("avatar");
+const uploadAvatar = document.getElementById("uploadAvatar"); //recupère le bouton upload
+const inputFile = document.querySelector("input[type=file]"); //recupère l'input file
+const avatar = document.getElementById("avatar"); //recupère la div avatar pour pouvoir mettre l'image d'avatar dedant
+
+uploadAvatar.addEventListener("click", () => inputFile.click());  //des qu'un click est sur le bouton on utilise input pour transmettre des fichiers
+//ici accepter (voir html)
+
+inputFile.addEventListener("change", event => {  
+    const file = event.target.files[0];
+  
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+  
+    reader.onloadend = () => {
+        avatar.setAttribute("aria-label", file.name);
+        avatar.style.background = `url(${reader.result}) center center/cover`;
+    };
+});
+
+
+
+// https://brains.hashnode.dev/upload-user-avatar-with-a-custom-upload-button
+// a revoir 
