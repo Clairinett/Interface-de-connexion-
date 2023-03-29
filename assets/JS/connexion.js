83085a574e9c2249ba2b4466707af065f23b01b7
[Clairinette] <[claire.crinier@gmail.com]>
2023-03-29 11:35:10 +0200

    inscription connexion ok mais que dans le js

--- assets/JS/connexion.js
+++ assets/JS/connexion.js
@@ -1,17 +1,21 @@
-const userdata8 = { 
-    email: "mail@mail.com",
-    password: "passwort",
-};
+// const userdata8 = { 
+//   username : "util8",
+//   email: "monmail@mail.com",
+//   password: "rickroll",
+// };
 
+const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2Nzk0Nzg3MzksImV4cCI6MTY3OTQ3ODc5OX0.mM-RoWVPr81UCXcJ_IkOb_uNzIJRNpCHqaznoFPXV9c"
 fetch("https://afpatraining.snage.tech/login", {
   method: "POST", 
   headers: {
     "Content-Type": "application/json",
-    "x-api-key" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2Nzk0Nzg3MzksImV4cCI6MTY3OTQ3ODc5OX0.mM-RoWVPr81UCXcJ_IkOb_uNzIJRNpCHqaznoFPXV9c",
-    "Authorization" : "$tokenuser",
+    "x-api-key": apiKey
   },
-//   redirect: "follow",
-  
+  body: JSON.stringify({
+    username: "jérémy",
+    email: "a@a.a",
+    password: "dazlidjazhdazdnczLDNHZAIUH",
+  }),
 })
 
 .then((response) => response.json())
--- assets/JS/inscription.js
+++ assets/JS/inscription.js
@@ -1,48 +1,41 @@
-// const url = 'https://afpatraining.snage.tech/signup';
-// const forminscription = document.getElementById('forminscription');
-// const sendinscritpion = document.getElementById('sendinscription');
+// const preventDefault = document.getElementsByClassName('preventDefault').addEventListener("click", function(event){
+//   event.preventDefault();
+// });
+
+// const setusername = document.getElementById("setusername");
+// const setemail = document.getElementById("setemail");
+// const setpassword = document.getElementById("setpassword");
+
+// const userdata8 = { 
+//   username: setusername,
+//   email: setemail,
+//   password: setpassword,
+// };
 
 
-// async function inscription(){
-//     const requete = await fetch(url, {
-//         method: "POST",
-//         headers: {
-//             "x-api-key" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2Nzk0Nzg3MzksImV4cCI6MTY3OTQ3ODc5OX0.mM-RoWVPr81UCXcJ_IkOb_uNzIJRNpCHqaznoFPXV9c",
-//             // "Content-Type" : "multipart/form-data"
-//             "Content-Type" : "application/json",
-//             "authorization" : "$tokenuser"
-//         },
-//         // body: new URLSearchParams ({
-//         // }),
-//         body: {
-//             username : "util8",
-//             mail : "monmail@mail.com",
-//             password : "rickroll",
-//         },
-//     });
 
-//     if(requete.ok) {
-//         alert("Un problème est survenu");
-//     }
-//     else {
-//         const donnees = await requete.json();
-//         console.log(donnees);
-//     }
-// }
 
-// inscription();
 
-const userdata8 = { 
-    username: "util8",
-    email: "monmail@gmail.com",
-    password: "rickroll",
-};
-
-// const userdata8 = {
+//id 10
+// const userdata8 = {    
 //     username : "utilisatrice8",
 //     email : "mail@mail.com",
 //     password : "passwort"
 // }
+
+//id 11
+// const userdata8 = { 
+//   username: "util8",
+//   email: "monmail@gmail.com", //attention une lettre horrible
+//   password: "rickroll",
+// };
+
+//id 13
+const userdata8 = { 
+  username: "jérémy",
+  email: "a@a.a",
+  password: "dazlidjazhdazdnczLDNHZAIUH",
+};
 
 fetch("https://afpatraining.snage.tech/signup", {
   method: "POST", 
